75bf6b483bd36dc2cd7d5317f7dff0eae39faa7e
Renato Janser <renato.janser@gmail.com>
2020-01-17 14:15:25 -0300

    Está salvando no banco mongodb o dev

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,32 @@
 const { Router } = require('express')
 const axios = require('axios')
+const Dev = require('./models/Dev')
 
 const routes = Router()
 
 routes.post('/devs', async (request, response) => {
-    const { github_username } = request.body
+    const { github_username, techs, latitude, longitude } = request.body
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
-    const { name = login, avatar_url, bio  } = apiResponse.data
+    const { name = login, avatar_url, bio } = apiResponse.data
 
-    console.log(name, avatar_url, bio)
+    const techsArray = techs.split(',').map(tech => tech.trim())
+
+    const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude] 
+    }
+
+    const dev = await Dev.create({
+        github_username,
+        name,   
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+    })
     
-    return response.json({ message: "Hello Janser..."})
+    return response.json(dev)
 })
 
 module.exports = routes
