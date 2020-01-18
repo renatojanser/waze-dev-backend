f2fff0cbe0cbb252d9596c027405cea61a239e33
Renato Janser <renato.janser@gmail.com>
2020-01-18 10:01:50 -0300

    Iniciei o metodo busca com os filtros

---
+++ src/controllers/SearchController.js
@@ -0,0 +1,11 @@
+const axios = require('axios')
+const Dev = require('../models/Dev')
+
+module.exports = {
+    async index(request, response) {
+        console.log(request.query)
+        
+        // Buscar todos devs bun raio 10km
+        // Filtrar por tecnologias
+    }
+}
