2beb2c9dc830a17e1330d09e1a9eb3df2bb1562c
Renato Janser <renato.janser@gmail.com>
2020-01-18 17:13:35 -0300

    Finalizei o metodo de busca

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,11 +1,31 @@
 const axios = require('axios')
 const Dev = require('../models/Dev')
+const parseStringAsArray = require('../Utils/parseStringAsArray')
 
 module.exports = {
+    // Buscar todos devs bun raio 10km
+    // Filtrar por tecnologias
     async index(request, response) {
-        console.log(request.query)
         
-        // Buscar todos devs bun raio 10km
-        // Filtrar por tecnologias
+        const { latitude, longitude, techs } = request.query
+
+        const techsArray = parseStringAsArray(techs)
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude]
+                    },
+                    $maxDistance: 10000,
+                }
+            }
+        })
+
+        return response.json({ devs })
     }
 }
